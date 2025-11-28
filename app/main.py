9ff0ebda5101539c76fc1cb93a32cb02bc74dc40
tcyxox <tcyxox@gmail.com>
2025-11-28 02:10:00 -0500

    Fix: expose header "ETag"

--- app/main.py
+++ app/main.py
@@ -68,6 +68,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=["ETag"]
 )
 
 # -----------------------------------------------------------------------------
